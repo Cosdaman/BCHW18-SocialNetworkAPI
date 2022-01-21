a2d16609bdf326082dadc8b4cbe757e00346a278
Cosdaman <Cosdaguy@gmail.com>
2022-01-20 21:23:26 -0800

    more placeholdefr

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -9,10 +9,10 @@ const {
     deleteReaction
 } = require('../../controllers/thoughtController')
 
-router.route('/').get().post();
+router.route('/').get(getThoughts).post(createThought);
 
-router.route(':_id').get().put().delete();
+router.route(':_id').get(getSingleThought).put(updateThought).delete(deleteThought);
 
-router.route(':thoughtId/reactions').post().delete();
+router.route(':thoughtId/reactions').post(createReaction).delete(deleteReaction);
 
 module.exports = router;
