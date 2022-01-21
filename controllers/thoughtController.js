80f3b39caeb651fb980410dacda018c4eed6261d
Cosdaman <Cosdaguy@gmail.com>
2022-01-20 21:01:55 -0800

    added more placeholders

---
+++ controllers/thoughtController.js
@@ -0,0 +1,25 @@
+const { Thought } = require('../models')
+
+module.exports = {
+    getThoughts(req, res) {
+
+    },
+    getSingleThought(req, res) {
+
+    },
+    createThought(req, res) {
+
+    },
+    updateThought(req, res) {
+
+    },
+    deleteThought(req, res) {
+
+    },
+    createReaction(req, res) {
+
+    },
+    deleteReaction(req, res) {
+
+    },
+}
---
+++ controllers/userController.js
@@ -0,0 +1,26 @@
+const { User } = require('../models')
+
+module.exports = {
+
+    getUsers(req, res) {
+
+    },
+    getSingleUser(req, res) {
+
+    },
+    createUser(req, res) {
+
+    },
+    updateUser(req, res) {
+
+    },
+    deleteUser(req, res) {
+
+    },
+    addUserFriend(req, res) {
+
+    },
+    removeUserFriend(req, res) {
+
+    }
+}
